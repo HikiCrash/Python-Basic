ecf386dcd5b1fb950bb61e4ebce8d63bf1626b99
HikiCrash <csh02127@naver.com>
2025-10-19 01:21:05 +0900

    P_003.py add

---
+++ Chapter(1)/P_003.py
@@ -0,0 +1,15 @@
+decimal = int(13) # 10진수
+binary = int(0b1101) # 2진수
+octal = int(0o15) # 8진수
+hexadecimal = int(0xD) # 16진수
+print(decimal, binary, octal, hexadecimal) # 전부 13 출력
+
+print("=" * 10)
+
+real = float(13.1)
+print(real)
+
+print("=" * 10)
+
+decimal = int(1_000_000_000) # 언더바로 자릿수 표시. 출력은 1000000000
+print(decimal)
