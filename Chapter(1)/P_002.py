ab8e3f86a2cb92c3c0eb61cc3740c826f4c373a0
HikiCrash <csh02127@naver.com>
2025-10-19 01:16:23 +0900

    P_002.py add

---
+++ Chapter(1)/P_002.py
@@ -0,0 +1,26 @@
+print() # 줄 바꿈
+print(int(1)) # 정수 1
+print(str("1")) # 문자열 1
+print(float(1.1)) # 실수 1.1
+print(bool(True)) # 참을 가짐
+print([1, 2, 3]) # 리스트 [1,2,3]
+print("123") # 문자열 123
+print("1" "2" "3") # 문자열 123
+print("1", "2", "3") # 문자열 1 2 3
+print("1" + "2" + "3") # 문자열연결연산 123
+
+print("=" * 10)
+
+print("I")
+print("AM")
+print("A")
+print("BOY.")
+
+print("I", end=" ") # end = ""은 문장 끝에 줄바꿈 대신에 ""사이에 있는 값을 출력
+print("AM", end=" ") 
+print("A", end=" ")
+print("BOY.", end="")
+
+print("=" * 10)
+
+print("I", "AM", "A", "BOY.", sep=";") # sep =""은 나열된 데이터 중간에 ""사이에 있는 값을 출력
