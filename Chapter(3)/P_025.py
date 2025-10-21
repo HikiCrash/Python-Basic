711634d17042d2e942d1e658eed20096297899dd
HikiCrash <csh02127@naver.com>
2025-10-21 20:10:53 +0900

    P_025.py add

---
+++ Chapter(3)/P_025.py
@@ -0,0 +1,38 @@
+cars = ["audi", "tesla", "benz", "kia", "lincoln", "hyundai"]
+print(cars)
+
+# 오름차순(정방향,asc)으로 정렬
+cars.sort() 
+print(cars) # ['audi', 'benz', 'hyundai', 'kia', 'lincoln', 'tesla']
+
+# 내림차순(역방향,desc)으로 정렬
+cars.sort(reverse=True) # False는 오름차순 True는 내림차순 sort() 기본형은 False
+print(cars) # ['tesla', 'lincoln', 'kia', 'hyundai', 'benz', 'audi']
+
+print("-" * 60)
+
+cars = ["audi", "tesla", "benz", "kia", "lincoln", "hyundai"]
+
+# 오름차순(정방향,asc)으로 정렬
+cars_copy = sorted(cars)
+print(cars_copy) # ['audi', 'benz', 'hyundai', 'kia', 'lincoln', 'tesla']
+
+# 내림차순(역방향,desc)으로 정렬
+cars_copy = sorted(cars, reverse=True)
+print(cars_copy) # ['tesla', 'lincoln', 'kia', 'hyundai', 'benz', 'audi']
+print(cars) # ['audi', 'tesla', 'benz', 'kia', 'lincoln', 'hyundai'] 원본 손상 없음
+
+print("-" * 60)
+
+cars = ["audi", "tesla", "benz", "kia", "lincoln", "hyundai"]
+cars.reverse() # 원본 손상
+print(cars) # ['hyundai', 'lincoln', 'kia', 'benz', 'tesla', 'audi']
+
+print("-" * 60)
+
+# 원본 손상없이 reverse()를 하려면?
+cars = ["audi", "tesla", "benz", "kia", "lincoln", "hyundai"]
+cars_copy = cars[:]  # 복사
+cars_copy.reverse()
+print(cars)
+print(cars_copy)
