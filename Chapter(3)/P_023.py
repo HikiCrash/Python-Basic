bf89c2dbfdfb5c7f7c85374a95a1a6f64ceadc73
HikiCrash <csh02127@naver.com>
2025-10-21 19:31:07 +0900

    P_023.py add

---
+++ Chapter(3)/P_023.py
@@ -0,0 +1,18 @@
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+d = [a, b]
+
+print(a) # [1, 2, 3]
+print(b) # [4, 5, 6]
+print(c) # [1, 2, 3, 4, 5, 6]
+print(d) # [[1, 2, 3], [4, 5, 6]]
+
+e = a * 3
+print(e) # [1, 2, 3, 1, 2, 3, 1, 2, 3]
+
+print(len(a)) # 3
+print(len(b)) # 3
+print(len(c)) # 6
+print(len(d)) # 2
+print(len(e)) # 9
