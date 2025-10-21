87c484fb3397603783ec779ddb7fa430de2be8be
HikiCrash <csh02127@naver.com>
2025-10-22 01:38:35 +0900

    P_036.py add

---
+++ Chapter(4)/P_036.py
@@ -0,0 +1,15 @@
+print("Good morning. Nice to meet you.")
+print("Where are you from?")
+print("Please select a number")
+choiceNumber = int(input("1.대한민국 2.USA 3.日本 4.中國 : "))
+
+if choiceNumber == 1:
+ print("주문하시겠어요?")
+elif choiceNumber == 2:
+ print("Would you like to order?")
+elif choiceNumber == 3:
+ print("注文しますか？")
+elif choiceNumber == 4:
+ print("您要訂購嗎？")
+else:
+ print("Would you like to order?")
