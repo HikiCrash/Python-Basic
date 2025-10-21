130560fd2859a6026cec51c881626c9f21fb2355
HikiCrash <csh02127@naver.com>
2025-10-22 01:39:25 +0900

    P_037.py add

---
+++ Chapter(4)/P_037.py
@@ -0,0 +1,10 @@
+peopleNumber = int(input("인원수:"))
+
+if peopleNumber == 1:
+ print("300,000원 지원")
+elif peopleNumber == 2:
+ print("500,000원 지원")
+elif peopleNumber == 3:
+ print("600,000원 지원")
+elif peopleNumber >= 4:
+ print("900,000원 지원")
