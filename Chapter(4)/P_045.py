64afa669457065420896a2a89c2084abfb172fdb
HikiCrash <csh02127@naver.com>
2025-10-22 02:57:39 +0900

    P_045.py add

---
+++ Chapter(4)/P_045.py
@@ -0,0 +1,13 @@
+# 리스트같은 길이 변경이 가능한 작업은 절대 for 쓰지 않는다.
+unconfirmed_users = ["김미영", "최용현", "이교수", "박미추"]
+confimred_users = []
+
+while unconfirmed_users: # 리스트가 빌 때까지 반복
+    current_user = unconfirmed_users.pop() # 뒤에서 부터 뽑기
+    valid = input(f"유효 유저:{current_user} (y/n) ").strip().lower() 
+    if valid == "y":
+        confimred_users.append(current_user)
+
+print("확인한 유저 목록입니다.")
+for users in confimred_users:
+    print(f" {users}")
