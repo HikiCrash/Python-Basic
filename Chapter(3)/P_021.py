22fa55170f882b8e656aae3a68c129d1528e21c7
HikiCrash <csh02127@naver.com>
2025-10-21 18:16:32 +0900

    P_021.py add

---
+++ Chapter(3)/P_021.py
@@ -0,0 +1,9 @@
+list_1 = list() # 빈 리스트
+list_2 = [] # 빈 리스트
+
+list_3 = list(["1", 2, 3.3]) 
+list_4 = ["1", 2, 3.3]
+
+list_5 = ["1", [2, 3.3]] # 중첩 리스트
+
+print(list_1, list_2, list_3, list_4, list_5)
