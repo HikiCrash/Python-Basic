cb0d967588b0989cb4cc0a5a810a442311304a48
HikiCrash <csh02127@naver.com>
2025-12-08 22:48:10 +0900

    P_068.py add

---
+++ Chapter(7)/P_068.py
@@ -0,0 +1,11 @@
+# 파일열기
+f = open("c:/myPython/test01.txt", "w") # 쓰기 모드는 파일이 없으면 생성, 있으면 모든 내용 삭제 후 새로 씀.
+# f = open("c:\\myPython\\test01.txt", 'w')
+# f = open(r"c:\myPython\test01.txt", 'w')
+# 위 3가지 코드는 모든 동일한 역할이나 표현방식만 다름.
+
+# 파일작업
+print(f)
+
+# 파일닫기
+f.close()
